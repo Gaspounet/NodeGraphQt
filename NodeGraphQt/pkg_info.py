42fd83dbd75fbe0c3194116307292e2e1e41deca
Gaspounet <gaspounet@gmail.com>
2024-09-16 19:09:10 -0400

    Updated the package info with fork information

--- NodeGraphQt/pkg_info.py
+++ NodeGraphQt/pkg_info.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-__version__ = '0.6.37'
+__version__ = '0.0.1'
 __status__ = 'Work in Progress'
 __license__ = 'MIT'
 
 __author__ = 'Johnny Chan'
+__maintainer__ = 'Gaspounet'
 
 __module_name__ = 'NodeGraphQt'
-__url__ = 'https://github.com/jchanvfx/NodeGraphQt'
+__url__ = 'https://github.com/Gaspounet/NodeGraphQt'
